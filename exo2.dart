f768ae998c3bfd091bc28d115f5257ebff032037
Hachem Souyadi <hachemdsq@gmail.com>
2025-11-15 04:01:04 -0500

    nouvelle modif

---
+++ exo2.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main(){  
+  stdout.write('entrez votre age'); 
+  var ageText = stdin.readLineSync();
+  try {
+    var age = int.parse(ageText!);
+    print("votre age est$age"); 
+  } 
+  on FormatException catch(e){ 
+    print("Format invalide$e"); 
+  }}
